f448d0e2bf4cb44904176a77c4d9c2b27b8f5952
Mahendra Kumawat <mahendrakumawat@Mahendras-MacBook-Air.local>
2025-07-03 17:14:19 +0530

    implement code spliting

--- src/Router/RouteConfig.jsx
+++ src/Router/RouteConfig.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router';
 import MainLayout from '@/MainLayout/MainLayout';
-import MainComponents from '@/components/custom/MainComponent/MainComponents';
+
+// Lazy load the MainComponents
+const MainComponents = lazy(() => import('@/components/custom/MainComponent/MainComponents'));
 
 const RouteConfig = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route index element={<MainComponents />} />
+        <Route index element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <MainComponents />
+          </Suspense>
+        } />
       </Route>
     </Routes>
   );
